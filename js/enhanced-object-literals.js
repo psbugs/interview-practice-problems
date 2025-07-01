dcfecebe722ba4d3e41ff646e91e677d1db69446
Praveen Sunhare <praveen786sunhare@gmail.com>
2025-07-01 11:10:03 +0530

    Enhanced object literal is done

---
+++ js/enhanced-object-literals.js
@@ -0,0 +1,18 @@
+function createLogger(level,type){
+    return {
+        level,
+        type,
+        log : function(message){
+            console.log(`[${level.toUpperCase()}]:[${type}]`,message)
+        },
+        [level+'Log'] : function(msg){
+            console.log(`>>> ${type.toUpperCase()} MESSAGE:`,msg)
+        }
+    }
+};
+
+let authLogger = createLogger('auth','info');
+
+authLogger.log('Data is saved successfully.');
+
+authLogger.authLog('Token is validated');
---
+++ js/longestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,7 @@
+var input = 'abcabcbb';
+
+function longestSubstring(){
+    return 10
+};
+
+console.log('Result',longestSubstring())
