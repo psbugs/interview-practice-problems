1a7787c00c90d751bb8a339a960c3dc82ce4c78d
Praveen Sunhare <praveen786sunhare@gmail.com>
2025-08-02 17:20:27 +0530

    added add remove item from cart logic.

--- js/concepts-implementation/closures.js
+++ js/concepts-implementation/closures.js
@@ -1,18 +1,18 @@
-function createCounter(){
-    let counter =0;
-    return {
-        increment : function(){
-             counter++;
-             return counter
-        },
-        decrement: function(){
-             counter--;
-             return counter
-        },
-        getCounter: function(){
-            return counter;
-        }
+function createCounter() {
+  let counter = 0;
+  return {
+    increment: function () {
+      counter++;
+      return counter
+    },
+    decrement: function () {
+      counter--;
+      return counter
+    },
+    getCounter: function () {
+      return counter;
     }
+  }
 }
 
 let counterObj = createCounter();
@@ -66,3 +66,36 @@ function authManager() {
 const auth = authManager();
 auth.login(); // User logged in
 console.log(auth.checkAuth()); // true
+
+
+
+
+function Cart() {
+  let carts = [];
+  return {
+    add: function (item) {
+      carts.push(item);
+      return 'Item is added to the cart';
+    },
+    remove: function (id) {
+      console.log('id', id);
+      carts = carts.filter((item) => item.id !== id);
+
+    },
+    getCart: function () {
+      return carts;
+    }
+  }
+};
+
+let cart = Cart();
+
+cart.add({ id: 1, productName: 'iphone13', price: 2 })
+cart.add({ id: 2, productName: 'cooler', price: 3 })
+cart.add({ id: 3, productName: 'hplaptop', price: 4 });
+
+console.log(cart.getCart())
+
+cart.remove(3);
+
+console.log(cart.getCart())
--- js/es8-2017.js
+++ js/es8-2017.js
@@ -1,11 +1,11 @@
 async function fetchUsers(params) {
-    const {id} = params;
-    return await [{id:1,name:'test'},{id:2,name:'test1'},{id:3,name:'test2'}]
+    const { id } = params;
+    return await [{ id: 1, name: 'test' }, { id: 2, name: 'test1' }, { id: 3, name: 'test2' }]
 }
-let response = fetchUsers({id:78}).then(data=> {
-console.log('data',data);
-}).catch(err=> {
-console.log('err',err);
+let response = fetchUsers({ id: 78 }).then(data => {
+    console.log('data', data);
+}).catch(err => {
+    console.log('err', err);
 });
 
 // Object.entries() and Object.values()
