7b09d03da20bcdab9088e80ee398d881559ea01d
Praveen Sunhare <praveen786sunhare@gmail.com>
2025-10-06 20:53:02 +0530

    completed walk in drive yash technology interview question and answers

--- company-wise-questions/yash-technology-2025-sept.js
+++ company-wise-questions/yash-technology-2025-sept.js
@@ -5,3 +5,42 @@
 
 // Duration of 40-45 mints
 
+// On backend which framework u have used , express js , nest js (Actually i am a mern stack developer that's why he asked me)
+// 1. Why is javascript is a dynamic typed programming language?
+// 2. Difference b/w var let and const
+// 3. What is temporal dead zone
+// 4. What is hoisting give examples 
+// 5. What is the use of closures if you are using closures what will happen , and if not use closures what happens
+// 6. What is the working of useEffect , useState , useDebounce custom hook , write syntax for it
+// 7. Write down the syntax for how to create a context in react js
+// 8. What is HOC , write its syntax for Logged in component
+// 9. Have u written unit test cases in your projects ?
+// 10. What is the purpose of useMemo as hook , what are its advantages
+// 11. What is the need of typescripts as a developer
+// 12. What is the difference between useReducer and useContext as a hook in a react js
+// 13. What are semantic elements give examples of various supported tags
+// 14 How do u make a site as responsive
+// 15. What is server side rendering how do u manage it in a react js project 
+// 16. Latest features of react 19
+// 17. What is the use of useTransition hook 
+// 18. What is box model
+// 19. What is the difference between relative, absolute , sticky , fixed in css?
+// 20. 
+
+
+
+// Second Round
+
+// 1. Panel had given me a counter component i need to find out the bug in that code
+// 2. How do you apply best practices for a react js application
+// 3. On pen and paper he asked me to write the html and css for a layout 
+// 4. Features of react 19 
+// 5. What are web components ?
+// 6. What is sonarcube
+// 7. Suppose in your application you need to centralized a loader how do u do that (unique solu he want )
+// 8. What is git rebase, git stash and git stash pop
+// 9. I want to resolve all es lint problems needs to be resolved then only i am going to push my changes, how can i do that
+// 10. How SEO works
+// 11. How do u maintain code consistency 
+
+// Finally I gets selected 
